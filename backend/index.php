fa60e5b8e39b8b6e21efc914d8bdb615386b4c35
Max Stewart <39015672+MaxStewart@users.noreply.github.com>
2019-08-08 15:37:43 +1200

    Linked two different vms

---
+++ backend/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Backend</title>
+</head>
+<body>
+
+<h1>Site for the Backend</h1>
+<p>Information processing will be done here</p>
+<p><a href="http://127.0.0.1:10222/">Frontend</a></p>
+
+</body>
+</html>
---
+++ frontend/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Frontend</title>
+</head>
+<body>
+
+<h1>Site for the Frontend</h1>
+<p>Forms to insert data into database will be here</p>
+<p><a href="http://127.0.0.1:10122/">Backend</a></p>
+
+</body>
+</html>
